da4c61bd8a1e84be9170364d5573b5cf2731a36d
Leandro Wiemes Filho <leandro.wiemesfilho@gmail.com>
2025-09-28 14:12:23 +0100

    Moved password configuration to SecurityConfig class to center all security configurations in one place. Further it can be analysed to be moved to another place to be common for all microservices

--- services/auth-service/src/main/java/com/chatapp/authservice/config/SecurityConfig.java
+++ services/auth-service/src/main/java/com/chatapp/authservice/config/SecurityConfig.java
@@ -3,11 +3,18 @@ package com.chatapp.authservice.config;
 import com.chatapp.security.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public JwtService jwtService() {
         return new JwtService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
